77f25bab4b57f09b160995d895b21f8906ebdd9a
MOUGIN Mehdi <mehdi.mougin@hotmail.fr>
2021-02-17 15:30:32 +0100

    cardV1

---
+++ assets/components/card.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="base">
+    <div class="headerInfo">
+        <div class="cloud">{{cloud}}</div>
+       <div class="humidity">{{humidity}}</div>
+       <div class="wind">{{wind}}</div>
+    </div>
+    <div class="bodyInfo">
+        <div class="temperature">{{temperature}}</div>
+        <div class="state">{{state}}</div>
+    </div>
+</div>
+
+
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+            cloud: "nuageux",
+            humidity:"40%",
+            wind:"70%",
+            temperature:"7°C",
+            state:"state"
+        }
+    }
+}
+</script>
