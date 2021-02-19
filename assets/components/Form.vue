db2a527242ade655b3525b521675f140e3d12834
Mehdi Sebbah <sebbah.m.dev@gmail.com>
2021-02-19 17:08:05 +0100

    Appli fonctionelle

--- assets/components/Form.vue
+++ assets/components/Form.vue
@@ -7,15 +7,15 @@
 </template> -->
 
 <template>
-  <div>
+  <div class="main">
     <div class="form">
       <form method="post" v-on:submit.prevent="callApi">
-        <label for="city">Ville</label>
-        <input type="text" name="city" v-model="city" />
+        <h1>MÉTÉHOME</h1>
+        <label for="city">Entrer une ville</label>
+        <input class="input_form" type="text" name="city" v-model="city" />
         <input type="submit" class="button" value="Valider" />
       </form>
       <div v-if="Object.keys(infoCity).length !== 0">
-        <p>{{ infoCity.data }}</p>
         <card v-bind:meteo="infoCity.data"></card>
       </div>
     </div>
@@ -24,16 +24,63 @@
 
 <style>
 .form {
+  margin-top: 30px;
+  font-family: "Spartan", sans-serif;
   display: flex;
-  flex-direction: row-reverse;
-  height: 50px;
+  flex-direction: column;
+  height: 100%;
+
 }
-.button2 {
-  width: 250px;
-  height: 200px;
-  border: 3px solid black;
-  padding: 20px 20px;
+
+h1 {
+  text-align: center;
+  color: white;
+  font-size: 2rem;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+   width: 350px;
+  height: auto;
+  margin: auto;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 2px 10px 15px #3333;
+  background-color: #8dd2d9;
+}
+
+form label {
+  color: white;
+  font-size: 2rem;
+}
+
+.input_form {
+  font-size: 1.5rem;
+  margin: 15px 0;
+  border: 2px solid white;
+  border-radius: 20px;
+  background-color: transparent;
+  color: white;
+  outline: none;
+}
+
+.button {
+  padding: 10px 25px;
+  font-size: 1.5rem;
+  border: none;
   border-radius: 15px;
+  background-color: white;
+  color: #8dd2d9;
+  cursor: pointer;
+  outline: none;
+  transition: 0.2s;
+}
+
+.button:hover {
+  opacity: 0.5;
+  transition: 0.2s;
 }
 </style>
 
